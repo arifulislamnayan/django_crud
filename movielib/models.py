5572695268b1cfe484df02e39642bbb2b3461b83
arifulislamnayan <farazi.aarif@gmail.com>
2015-01-29 15:56:59 +0600

    models.py changed

--- movielib/models.py
+++ movielib/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=128)
+    title = models.CharField(max_length=255)
     genre = models.CharField(max_length=128)
     release_date = models.DateField()
     price = models.DecimalField(max_digits=20, decimal_places=0)
